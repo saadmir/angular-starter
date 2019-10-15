11a8dca1e114f6e01786826ee24f5c91ee3d5550
Saad Mir <saadmir@gmail.com>
2019-10-14 20:44:16 -0700

    gh-pages

---
+++ src/app/rest.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+// import {Observable} from "rxjs/Observable";
+import {HttpClient} from '@angular/common/http';
+import * as _ from 'lodash';
+import { map, flatMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+const BASE_URL = 'http://code.saadmir.com/api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  constructor(private http: HttpClient) { }
+
+  doGet(endpoint: string = ''): Observable<any> {
+    return this.http.get<any[]>(`${BASE_URL}/${endpoint}`).pipe(
+      map((data) => data)
+    );
+  }
+
+}
