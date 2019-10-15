cb38cb134922231727195971056cb8828b5cb06f
Saad Mir <saadmir@gmail.com>
2019-10-14 20:42:55 -0700

    gh-pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map, flatMap } from 'rxjs/operators';
+
+import { RestService } from './rest.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
-export class AppComponent {
+export class AppComponent  implements OnInit {
   showMenu = false;
 
-  constructor() {}
-
-  ngOnInit() {
-    console.log('[dashboard.component]', 'ngOnInit');
+  constructor(private restService: RestService){
   }
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
+  }
+
+  ngOnInit() {
+    this.restService.doGet('/users').subscribe((res) => {
+      console.log('res', res);
+    });
+
   }
 }
